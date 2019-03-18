b0577a7df0284323ee2633f212827807b755a792
Andrew McLelland <andrew.mclelland23@gmail.com>
2019-03-18 17:09:30 +0000

    Completed the 'Sinatra: posted params' step

--- lib/app.rb
+++ lib/app.rb
@@ -12,7 +12,18 @@ get '/yoda' do
   "Hello Yoda"
 end
 
-get '/cat' do
+get '/random-cat' do
   @random_name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
+end
+
+post '/named-cat' do
+  p params
+  @random_name = params[:name]
+  @random_food = params[:food]
+  erb(:index)
+end
+
+get '/cat-form' do
+  erb(:cat_form)
 end
